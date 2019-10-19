579d95155a5465194272610115cf60ff07cd39ac
Norbert Wagner <wagner.norbert277@gmail.com>
2019-10-20 00:16:49 +0200

    Updated README

--- elementaryMathOperators.go
+++ elementaryMathOperators.go
@@ -41,3 +41,16 @@ func isIntEven(a int) bool {
 	}
 	return answerIE
 }
+func reducingFloatByPercent(a float64, pUser float64) float64 {
+	p := pUser / 100
+	percentOfA := p * a
+	reduceF := a - percentOfA
+	return reduceF
+}
+
+func reducingIntByPercent(a int, pUser int) int {
+	p := pUser / 100
+	percentOfA := p * a
+	reduceI := a - percentOfA
+	return reduceI
+}
