78caa0e59640128a43af7dd5d360748459363d11
Norbert Wagner <wagner.norbert277@gmail.com>
2019-10-20 12:04:32 +0200

    SimpleTest

---
+++ test/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nProgrammer/MathLib"
+)
+
+func main() {
+	fmt.Println(MathLib.ToThePowerInt(3, -1))
+	fmt.Println(MathLib.PI())
+	fmt.Println(MathLib.SqrtFloat(12))
+}
