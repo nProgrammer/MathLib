c86c265903a36ca447e8c870c0485ca16f365b90
Norbert Wagner <wagner.norbert277@gmail.com>
2019-10-20 11:36:44 +0200

    ADDED ConstantMathValues and PI

--- libMainFile.go
+++ libMainFile.go
@@ -1,7 +1,17 @@
 package MathLib
 
+type ConstantMathValues struct {
+	PI float64
+}
+
 func AddFloat(a float64, b float64) float64 {
 	return a + b
+}
+
+func PI() float64 {
+	constantMathValues := ConstantMathValues{}
+	constantMathValues.PI = 3.14
+	return constantMathValues.PI
 }
 
 func AddInt(a int, b int) int {
@@ -65,15 +75,29 @@ func SqrtInt(a int) int {
 }
 
 func ToThePowerInt(a int, n int) int {
-	score := a
+	scorePI := a
 	if n >= 1 {
 		for i := 1; i < n; i++ {
-			score = score * a
+			scorePI = scorePI * a
 		}
 	} else if n == 0 {
-		score = 1
+		scorePI = 1
 	} else {
-		score = 0
+		scorePI = 0
 	}
-	return score
+	return scorePI
+}
+
+func ToThePowerFloat(a float64, n float64) float64 {
+	scorePF := a
+	if n >= 1 {
+		for i := 1; i < n; i++ {
+			scorePF = scorePF * a
+		}
+	} else if n == 0 {
+		scorePF = 1
+	} else {
+		scorePF = 0
+	}
+	return scorePF
 }
